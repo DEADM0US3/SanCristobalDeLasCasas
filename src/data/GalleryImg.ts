3791cbf473e7b3b4fd314dd017feea4b08995e25
joelrod12 <106689717+joelrod12@users.noreply.github.com>
2025-05-25 10:25:53 -0500

    array de imagenes
    array de imagenes con categorias

---
+++ src/data/GalleryImg.ts
@@ -0,0 +1,179 @@
+export interface Gallery {
+    src: string
+    alt: string
+    category: 'lugares' | 'restaurante' | 'festividades' | 'Cultura' | 'Home' | 'tours' | 'comida' | 'hotel'
+}
+
+export const GalleryImg: Gallery[] = [
+    {
+        src: '/img/Restaurantes.webp',
+        alt: 'restaurante rustico',
+        category: 'restaurante'
+    },
+    {
+        src: '/img/homeImage.webp',
+        alt: 'Home',
+        category: 'Home'
+    },
+    {
+        src: '/img/Festividades.webp',
+        alt: 'festividades',
+        category: 'festividades'
+    },
+    {
+        src: '/img/mar.webp',
+        alt: 'mar',
+        category: 'lugares'
+    },
+    {
+        src: '/img/cueva.webp',
+        alt: 'caverna',
+        category: 'lugares'
+    },
+    {
+        src: '/img/cascada.webp',
+        alt: 'cascada',
+        category: 'lugares'
+    },
+    {
+        src: '/img/estructuras.webp',
+        alt: 'castillo',
+        category: 'lugares'
+    },
+    {
+        src: '/img/cabaña.webp',
+        alt: 'cabaña',
+        category: 'lugares'
+    },
+    {
+        src: '/img/calles.webp',
+        alt: 'calles',
+        category: 'lugares'
+    },
+    {
+        src: '/img/calles3.webp',
+        alt: 'calles3',
+        category: 'lugares'
+    },
+    {
+        src: '/img/iglesia.webp',
+        alt: 'iglesia',
+        category: 'lugares'
+    },
+    {
+        src: '/img/lugar1.webp',
+        alt: 'cueva',
+        category: 'lugares'
+    },
+    {
+        src: '/img/lugar2.webp',
+        alt: 'fuentes',
+        category: 'lugares'
+    },
+    {
+        src: '/img/calles2.webp',
+        alt: 'calles',
+        category: 'lugares'
+    },
+    {
+        src: '/img/mascara.webp',
+        alt: 'mascara',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/iglesia2.webp',
+        alt: 'iglesia',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/ranas.webp',
+        alt: 'ranas',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/cocina.webp',
+        alt: 'cocina',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/cultura.webp',
+        alt: 'cultura',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/cultura2.webp',
+        alt: 'cultura2  ',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/cultura3.webp',
+        alt: 'cultura3  ',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/cultura4.webp',
+        alt: 'cultura4  ',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/cultura5.webp',
+        alt: 'cultura5  ',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/cultura6.webp',
+        alt: 'cultura6  ',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/piramides.webp',
+        alt: 'piramide  ',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/baile.webp',
+        alt: 'baile  ',
+        category: 'Cultura'
+    },
+    {
+        src: '/img/tours1.webp',
+        alt: 'tours1',
+        category: 'tours'
+    },
+    {
+        src: '/img/guacamayas.webp',
+        alt: 'guacamayas',
+        category: 'tours'
+    },
+    {
+        src: '/img/comida.webp',
+        alt: 'tacos',
+        category: 'comida'
+    },
+    {
+        src: '/img/hotel1.webp',
+        alt: 'hotel1',
+        category: 'hotel'
+    },
+    {
+        src: '/img/hotel2.webp',
+        alt: 'hotel2',
+        category: 'hotel'
+    },
+    {
+        src: '/img/hotel3.webp',
+        alt: 'hotel3',
+        category: 'hotel'
+    },
+    {
+        src: '/img/hotel4.webp',
+        alt: 'hotel4',
+        category: 'hotel'
+    },
+    {
+        src: '/img/hotel5.webp',
+        alt: 'hotel5',
+        category: 'hotel'
+    },
+
+]
