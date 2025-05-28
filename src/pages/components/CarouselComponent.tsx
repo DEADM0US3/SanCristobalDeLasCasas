a8d45592f5ad10b8ac94a8f46f4f8b653d94570a
joelrod12 <106689717+joelrod12@users.noreply.github.com>
2025-05-28 18:30:59 -0500

    Update CarouselComponent.tsx

--- src/pages/components/CarouselComponent.tsx
+++ src/pages/components/CarouselComponent.tsx
@@ -1,21 +1,48 @@
 
-import {Carousel} from "primereact/carousel";
-import { GalleryImg } from '../../data/GalleryImg';
+import { Carousel } from 'primereact/carousel';
+import { GalleryImg, Gallery } from '../../data/GalleryImg';
+import { useEffect, useState } from 'react';
 
-const carouselTemplate = (object) => {
-    return (
-        <div style={{backgroundImage: `url(${object.src})`}} className={`border-1 h-52 bg-repeat-round surface-border border-round m-2 text-center py-5 px-3`} >
-         
-        </div>
-    );
+export const GalleryService = {
+
+    getImagesData() {
+        return GalleryImg;
+    },
+
+    getImages() {
+        return Promise.resolve(this.getImagesData());
+    },
+
+}
+
+
+const carouselTemplate = (object: Gallery) => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${object.src})` }}
+      className="border-1 h-52 bg-cover bg-center surface-border border-round m-2 text-center py-5 px-3 flex items-end justify-center"
+    >
+    </div>
+  );
 };
 
 
-const CarouselComponent = () =>{
-    return(
+
+const CarouselComponent = () => {
+
+    const [products, setProducts] = useState<Gallery[]>([]);
+
+    useEffect(() => {
+        GalleryService.getImages().then((data) => setProducts(data.slice(0, 32)));
+    }, []);
+
+
+    //bg-[#1a1a1e]
+
+    return (
         <>
-            <div className="bg-[#1a1a1e] z- w-[100%] mb-10 h-[40dvh] flex flex-col justify-center items-center p-5">
-                <Carousel value={GalleryImg} numVisible={4} numScroll={1} itemTemplate={carouselTemplate} />
+            <div className=" z- w-[100%] mb-10 h-[40dvh] flex flex-col justify-center items-center p-5">
+                <Carousel className="custom-carousel" value={products} numVisible={4} numScroll={1} showIndicators={true} itemTemplate={carouselTemplate} />
             </div>
         </>
     )
