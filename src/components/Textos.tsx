581a596b8bed1ef5511a9cf2486086ea0163d2cb
DEADM0US3 <ffernando.villafanaalfonseca@gmail.com>
2025-07-04 15:03:25 -0600

    finish

--- src/components/Textos.tsx
+++ src/components/Textos.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import logo from '../../img/logo.png';
 // const logo = require('../../img/logo.png');
 
 interface Props {
@@ -10,15 +10,16 @@ interface Props {
 const Texto: React.FC<Props> = ({ titulo, descripcion }) => {
     return (
         <>
-            <div className="relative w-full h-full px-2 sm:px-6 lg:px-14 py-8 flex flex-col justify-center text-white z-10 max-w-3xl">
+            <div
+                className="absolute -top-26 left-40 w-full h-full px-2 sm:px-6 lg:px-14 py-8 flex flex-col justify-center text-white z-10 max-w-3xl">
 
-                {/* <img src={logo} alt="Pueblos Mágicos" className="w-40 mb-4" /> */}
+                 <img src={logo} alt="Pueblos Mágicos" className="w-40" />
 
-                <h1 className="font-[Montserrat, sans-serif] text-4xl font-extrabold tracking-tight mb-2 ">
+                <h1 className="font-[Montserrat, sans-serif] text-5xl font-extrabold tracking-tight mb-2 ">
                     {titulo}
                 </h1>
 
-                <p className="text-base lg:text-lg text-white/90 montserrat">
+                <p className="text-base lg:text-xl text-white/90 montserrat">
                     {descripcion}
                 </p>
             </div>
