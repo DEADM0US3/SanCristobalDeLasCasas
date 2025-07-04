11d9ba3d65662b5422f9211cbbd5f2a6a3a7e2f0
DEADM0US3 <ffernando.villafanaalfonseca@gmail.com>
2025-07-04 15:21:10 -0600

    fe

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router";
+import {createHashRouter, RouterProvider} from "react-router";
 import appRoute from "./AppRoutes.tsx";
 
-const router = createBrowserRouter(appRoute);
+const router = createHashRouter(appRoute);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
